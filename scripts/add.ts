d8dacadd043558ea8c4ed720627c185b2052504b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-29 19:38:16 -0600

    1k

--- scripts/add.ts
+++ scripts/add.ts
@@ -1,9 +1,20 @@
 import allNames from "all-the-package-names";
+import fs from "node:fs";
 
 const allNameArray: string[] = allNames as string[]; 
 const MAX_DEPENDENCIES = 1000;
 
-// add the first 1k packages to the dependency package.json
 const packageNames = allNameArray.slice(0, MAX_DEPENDENCIES);
-console.log(packageNames.join("\n"));
 
+console.log("cwd: ", process.cwd());
+// write the packages to the package.json
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+
+packageJson.dependencies = packageJson.dependencies || {};
+
+packageNames.forEach((name) => {
+  console.log(`Adding ${name} to package.json`);
+  packageJson.dependencies[name] = "latest";
+});
+
+fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
